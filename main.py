f7cae59237d4423b29e83f8ed7c10aff54d7d928
Hard_worker <gl.arseniy@mail.ru>
2022-12-15 23:58:26 +0300

    Finaly working post and awesome bd

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ class User(db.Model):
     nickname = db.Column(db.String(40))
     salt = db.Column(db.String(100))
     password_hash = db.Column(db.String(40))
-    # places = db.relationship('Place', backref='user')
+    places = db.relationship('Place', backref='user')
     jobs = db.relationship('Job', backref='user')
-    # durations = db.relationship('Route', backref='user')
+    durations = db.relationship('Route', backref='user')
 
 
 class Place(db.Model):
@@ -26,7 +26,6 @@ class Place(db.Model):
     place_name = db.Column(db.String(40))
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     jobs = db.relationship('Job', backref='place')
-#     route = db.relationship('Place', secondary=Route,  backref='place')
 
 
 class Job(db.Model):
@@ -36,14 +35,14 @@ class Job(db.Model):
     job_duration = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     place_id = db.Column(db.Integer, db.ForeignKey('place.id'))
-#
-#
-# class Route(db.Model):
-#     duration_id = db.Column(db.Integer, primary_key=True)
-#     route_duration = db.Column(db.Integer)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-#     place_id_1 = db.Column(db.Integer, db.ForeignKey('place.place_id'))
-#     place_id_2 = db.Column(db.Integer, db.ForeignKey('place.place_id'))
+
+
+class Route(db.Model):
+    duration_id = db.Column(db.Integer, primary_key=True)
+    route_duration = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    place_id_1 = db.Column(db.Integer)
+    place_id_2 = db.Column(db.Integer)
 
 
 @app.route('/registration', methods=["POST"])
@@ -53,20 +52,11 @@ def reg():
     salt = request.json["salt"]
     password_hash = request.json["password_hash"]
 
-    print(user_token, nickname, salt, password_hash)
     user = User(user_token=user_token, nickname=nickname, salt=salt, password_hash=password_hash)
 
     db.session.add(user)
     db.session.commit()
     return {"user_token": user_token}
-
-
-@app.route('/user/job', methods=["GET"])
-def test():
-    user_token = request.headers["user_token"]
-    user = User.query.filter(user_token == user_token).first()
-    print(user.jobs)
-    return {"Lovi aptechku": str(user.jobs)}
 
 
 @app.route('/places', methods=["POST"])
@@ -76,30 +66,25 @@ def new_place():
     user_token = request.headers["user_token"]
     user = User.query.filter(User.user_token == user_token).first()
 
-    print(place_id, place_name, user.user_id)
     place = Place(place_id=place_id, place_name=place_name, user_id=user.user_id)
 
     try:
         db.session.add(place)
         db.session.commit()
+    except:
+        return abort(500)
+
+    try:
+        adjacent_place = user.places
+        for ad_place in adjacent_place:
+            if place_id == ad_place.place_id:
+                continue
+            route = Route(route_duration=0, user_id=user.user_id, place_id_1=place.id, place_id_2=ad_place.id)
+            db.session.add(route)
+            db.session.commit()
         return {"place_id": place_id}
     except:
         return abort(500)
-#
-#     adjacent_place = Place.query.filter_by(user_id=user_id).all()
-#
-#     try:
-#         for ad_place in adjacent_place:
-#             if place_id == ad_place.place_id:
-#                 continue
-#             duration_id = int(uuid.uuid4())
-#             route = Route(duration_id=duration_id, route_duration=0, user_id=user_id, place_id_1=place_id,
-#                           place_id_2=ad_place.place_id)
-#             db.session.add(route)
-#             db.session.commit()
-#         return {"place_id": place_id_str}
-#     except:
-#         return abort(500)
 
 
 @app.route('/jobs', methods=["POST"])
