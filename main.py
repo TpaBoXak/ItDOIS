74d4b904f55b61629144b94dc2e372618e48d0fc
Hard_worker <gl.arseniy@mail.ru>
2022-12-18 13:48:22 +0300

    Norm bd

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uuid
 import itertools
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ItDoLS.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
